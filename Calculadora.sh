33cfd53a6cf5bb1edbae06c8743b0598f4fb54ca
Ojferreira <orlandojferr@gmail.com>
2025-01-09 21:37:05 -0300

    Calculadora em shell script

---
+++ Calculadora.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Calculadora Simples"
+echo "Escolha uma operação: +, -, *, /"
+read operacao
+echo "Digite o primeiro número:"
+read num1
+echo "Digite o segundo número:"
+read num2
+
+case $operacao in
+  "+") resultado=$(echo "$num1 + $num2" | bc);;
+  "-") resultado=$(echo "$num1 - $num2" | bc);;
+  "*") resultado=$(echo "$num1 * $num2" | bc);;
+  "/") resultado=$(echo "scale=2; $num1 / $num2" | bc);;
+  *) echo "Operação inválida"; exit 1;;
+esac
+
+echo "Resultado: $resultado"
